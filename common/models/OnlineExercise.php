7401217b7c06f83507f765fa083a5c64fed36a97
guofeng <guofeng@heiwangke.com>
2019-10-31 18:10:24 +0800

    练习接口

--- common/models/OnlineExercise.php
+++ common/models/OnlineExercise.php
@@ -9,4 +9,9 @@
         {
             return "{{%online_exercise}}";
         }
+
+        public function getComment()
+        {
+            return $this->hasMany(Comment::class, ['to_id'=>'id'])->select('id, parent_id, user_id, to_id, type, content');
+        }
     }
